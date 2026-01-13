95af1436d1598c913232da15f9bd92f7f31131fd
Tertulia Libreria <tertulia-libreria@outlook.com>
2026-01-12 18:24:01 -0600

    Agrega Google Analytics externo

---
+++ analytics.js
@@ -0,0 +1,5 @@
+// Google Analytics - Tertulia Librería
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-F9HV4XFVCH');
